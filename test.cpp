5391ad4c7dc769053cd0abb341f97b28791e04f0
NotRistu <nathan.nguyen9957@gmail.com>
2020-09-18 01:23:16 -0500

    formatted test.cpp

--- test.cpp
+++ test.cpp
@@ -7,88 +7,92 @@ using namespace std;
 struct node
 {
     int number;
-    node* next;
+    node *next;
 };
 
-node* split(node*);
-node* merge(node*, node*);
-node* mergesort(node*);
+node *split(node *);
+node *merge(node *, node *);
+node *mergesort(node *);
 
 void parenthesis(string &);
 int useOp(int, int, char);
-node* operation(string);
+node *operation(string);
 
 int main()
 {
-    node* list_node = new node();
+    node *list_node = new node();
     string test = "2+{[(6-9)+9]-4}";
     parenthesis(test);
     list_node = operation(test);
-    list_node -> next = nullptr;
-    cout << list_node -> number;
+    list_node->next = nullptr;
+    cout << list_node->number;
     return 0;
 }
 
-
-node* mergesort(node* start)
+node *mergesort(node *start)
 {
-    if (start == nullptr || start -> next == nullptr)//recursion condition, and checks if list has 1-no nodes
+    if (start == nullptr || start->next == nullptr) //recursion condition, and checks if list has 1-no nodes
     {
         return start;
     }
 
-    node* head = start;
-    node* a;
-    node* b;
+    node *head = start;
+    node *a;
+    node *b;
 
-    node* middle = split(start);//splits list to 2 and assign the right pointers
-    node* middle1 = middle -> next;
-    middle -> next = nullptr;
+    node *middle = split(start); //splits list to 2 and assign the right pointers
+    node *middle1 = middle->next;
+    middle->next = nullptr;
 
-    node* left = mergesort(start);//splits the pieces even more to 2 units and compares them
-    node* right = mergesort(middle1);
+    node *left = mergesort(start); //splits the pieces even more to 2 units and compares them
+    node *right = mergesort(middle1);
 
-    return merge(left, right);//puts the list together recursively
-
+    return merge(left, right); //puts the list together recursively
 }
 
-node* split(node* list)
+node *split(node *list)
 {
-    node* slow;//slow goes 1 unit, and fast goes 2
-    node* fast;
+    node *slow; //slow goes 1 unit, and fast goes 2
+    node *fast;
     slow = fast = list;
 
-    while (fast -> next != nullptr && fast -> next -> next)
+    while (fast->next != nullptr && fast->next->next)
     {
-        slow = slow -> next;
-        fast = fast -> next -> next;
+        slow = slow->next;
+        fast = fast->next->next;
     }
     return slow;
 }
 
-node* merge(node* left, node* right) //compares and puts it back together
+node *merge(node *left, node *right) //compares and puts it back together
 {
-    node* temp = new node();
-    node* res = temp;
+    node *temp = new node();
+    node *res = temp;
 
     while (left != nullptr && right != nullptr)
     {
-        if (left -> number < right -> number)
+        if (left->number < right->number)
         {
-            res -> next = left;
-            left = left -> next;
+            res->next = left;
+            left = left->next;
         }
         else
         {
-            res -> next = right;
-            right = right -> next;
+            res->next = right;
+            right = right->next;
         }
-        res = res -> next;
+        res = res->next;
     }
-    if (left != nullptr){res -> next = left;}
-    if (right != nullptr){res -> next = right;}
+    if (left != nullptr)
+    {
+        res->next = left;
+    }
+    if (right != nullptr)
+    {
+        res->next = right;
+    }
 
-    return temp -> next;
+    return temp->next;
 }
 
 void parenthesis(string &s) //replaces the brackets and squiggly brackets with parenthesis to operate
@@ -109,25 +113,55 @@ void parenthesis(string &s) //replaces the brackets and squiggly brackets with p
     return;
 }
 
-node* operation (string expression)//does math and returns it as a node with data
+node *operation(string expression) //does math and returns it as a node with data
 {
-    node* result = new node();
+    node *result = new node();
     int total = 0;
     vector<int> numbers;
     vector<char> operators;
 
     for (char i : expression)
     {
-    if(i == '(') {operators.push_back(i);}//checks for open brace
-    else if (isdigit(i)){numbers.push_back(i - 48);}//checks for number
-    else if (i == ')')
-    {
-        //if loop finds closing brace, uses all numbers inside brace to evaluate
-        while(!operators.empty() && operators.back() != '(')
+        if (i == '(')
         {
+            operators.push_back(i);
+        } //checks for open brace
+        else if (isdigit(i))
+        {
+            numbers.push_back(i - 48);
+        } //checks for number
+        else if (i == ')')
+        {
+            //if loop finds closing brace, uses all numbers inside brace to evaluate
+            while (!operators.empty() && operators.back() != '(')
+            {
+                int a = numbers.back();
+                numbers.pop_back();
+
+                int b = numbers.back();
+                numbers.pop_back();
+
+                char op = operators.back();
+                operators.pop_back();
+
+                numbers.push_back(useOp(a, b, op));
+            }
+
+            if (!operators.empty()) //gets rid of open brace
+                operators.pop_back();
+        }
+        else //adds operator to empty vector
+        {
+            operators.push_back(i);
+        }
+    }
+
+    //apply the rest of the operators outside of parenthesis
+    while (!operators.empty())
+    {
         int a = numbers.back();
         numbers.pop_back();
-        
+
         int b = numbers.back();
         numbers.pop_back();
 
@@ -135,84 +169,26 @@ node* operation (string expression)//does math and returns it as a node with dat
         operators.pop_back();
 
         numbers.push_back(useOp(a, b, op));
-        }
-
-        if(!operators.empty())//gets rid of open brace
-            operators.pop_back();
-
-    }
-    else//loop finds a regular operator and evaluates it
-    {
-        // while(!operators.empty())
-        // {
-        //     int a = numbers.back();
-        //     numbers.pop_back();
-        
-        //     int b = numbers.back();
-        //     numbers.pop_back();
-
-        //     char op = operators.back();
-        //     operators.pop_back();
-
-        //     numbers.push_back(useOp(a, b, op));
-        // }
-
-        // operators.push_back(i);
-    
     }
 
-}
-
-
-//apply the rest of the operators
-while(!operators.empty())
-{
-    int a = numbers.back();
-    numbers.pop_back();
-        
-    int b = numbers.back();
-    numbers.pop_back();
-
-    char op = operators.back();
-    operators.pop_back();
-
-    numbers.push_back(useOp(a, b, op));
-}
-
-
-    total = numbers.back();
-    result -> number = total;
+    total = numbers.back(); //adds the sum to node number
+    result->number = total;
 
     return result;
-
 }
 
-int useOp (int left, int right, char op)
+int useOp(int left, int right, char op)
 {
     switch (op)
     {
-        case '+' : return left + right; break;
-        case '-' : return left - right; break;
-        default : return 0;
+    case '+':
+        return left + right;
+        break;
+    case '-':
+        return left - right;
+        break;
+    default:
+        return 0;
     }
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
