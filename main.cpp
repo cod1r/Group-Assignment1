95e9cf023d2a8973c4c517b22318935af458debe
Jason Ho <jasonho353@gmail.com>
2020-09-23 00:17:42 -0500

    all test cases passed

--- main.cpp
+++ main.cpp
@@ -10,123 +10,71 @@ using namespace std;
 struct node
 {
     int number;
-    node* next;
+    node* next = nullptr;
+	~node(){
+		delete next;
+	}
 };
+
+void print(node **head){
+	node* curr=*head;
+	while(curr!=nullptr){
+		cout << curr->number << " ";
+		curr=curr->next;
+	}
+	cout << endl;
+}
 
 void createlist(node** head, int num){
 	node* curr=*head;
 	node*temp=new node();
 	temp->number=num;
-	temp->next=nullptr;
-	if(*head == nullptr){
-		*head=temp;
-		temp=nullptr;
-	}
-	else{
-		
-		if(curr->next!=nullptr){	//goes to next node recursively
+	if (*head == nullptr){
+		*head = temp;
+		curr = temp;
+	} else {
+		if (curr->number > temp->number){
+			temp->next = curr;
+			*head = temp;
+		}
+		else if (curr->next != nullptr && curr->next->number < temp->number){
 			createlist(&curr->next, num);
+		} else {
+			node* tmp = curr->next;
+			curr->next = temp;
+			temp->next = tmp;
 		}
-		else
-			curr->next=temp;
 	}
-	
 }
 
-void deletepos_neg(node**head , node**head2, int x) { //delete pose/neg pairs
-	node* prev = nullptr; //create prev node
-	node* curr = *head; //create current node
-	node* prev2 = nullptr;
-	node* curr2 = *head2;
-	int i=0 ,j=0, c=0;
-	if (curr == nullptr )
+void addNode(node*& head, int num){
+	node* newN = new node;
+	newN->number = num;
+	node* copy = head;
+	if (head == nullptr){
+		head = newN;
 		return;
-	while (curr != nullptr){
-		if ((curr->number == x || curr->number + x == 0) && i<2 ) //compare
-		{
-			c++;
-			i++;
-			if (curr==*head) { //if it's the first node, set to next node
-				*head=(*head)->next;
-				curr = *head;
-				prev = *head;
-			}
-			else { //if not first node, skip the number
-				prev->next = curr->next; 
-				curr = curr->next;
-			}
+	} else {
+		while(copy->next != nullptr){
+			copy = copy->next;
 		}
-		else {
-			prev = curr; //if curr->id is not line, then set curr node to prev
-			curr = curr->next; //go to next node
-			
-		}
+		copy->next = newN;
 	}
-	//return if found pair
-	if (curr2 == nullptr || i==2)
+}
+
+void delete_invalid(int pos, node* head, int c) {
+	if (head == NULL )
 		return;
-	//if have not found pair yet, check second list
-	while (curr2 != NULL){
-			if ((curr2->number == x || curr2->number + x == 0) && j < 1 && c <= 1) 
-			{	
-				c++;
-				j++;
-				if (curr2 == *head2) {
-					*head2 = (*head2)->next;
-					curr2 = *head2;
-					prev2 = *head2;
-				}
-				else {
-					prev2->next = curr2->next; 
-					curr2 = curr2->next;
-				}
-			}
-			else {
-				prev2 = curr2; 
-				curr2 = curr2->next; 
-			}
-		
-		}
-}
-
-void checkneg(node**head, node**head2){ //check for negatives in each list. if negative then call delete function
-node*curr=*head;
-node*curr2=*head2;
-while(curr != nullptr){
-	if(curr->number<0)
-		deletepos_neg(&*head, &*head2,curr->number);
-	curr=curr->next;
-	
- 	}
-while(curr2!=nullptr){
-	if(curr2->number<0)
-		deletepos_neg(&*head2, &*head, curr2->number);
-	curr2=curr2->next;
- 	}
-}
-
-void delete_invalid(int pos, node** head, int c) {
-		node* curr = *head; //create current node and set head to curr
-		if (curr == NULL )
-			return;
-		if (curr!= NULL && c < pos) { //goes to next node recursively until position
-			c++;
-			delete_invalid(pos,&curr->next, c); //move to next node
-		}
-		else{
-			node* prev = curr->next->next; //set prev to the node after next node
-			delete curr->next; //free memory
-			curr->next = prev; //set prev node to next node of curr node
-		}
+	if (head != NULL && c < pos) { //goes to next node recursively until position
+		c++;
+		delete_invalid(pos, head->next, c); //move to next node
 	}
-
-void print(node **head){
-	node* curr=*head;
-	while(curr!=nullptr){
-		cout<<curr->number<<" ";
-		curr=curr->next;
+	else if (pos == c){
+		node* prev = head->next->next; //set prev to the node after next node
+		head->next = prev; //set prev node to next node of curr node
 	}
 }
+
 
 // jason's expression validator
 bool expressValid(string expression){
@@ -250,13 +198,134 @@ int operation(string expression) //does math and returns it as a node with data
 
         numbers.push_back(useOp(a, b, op));
     }
-
-    return numbers.back();
+    return numbers[numbers.size()-1];
 }
-// jason's future merge function
+
+node* merge(node* one, node* two){
+	node* ret = nullptr;
+	node* head = nullptr;
+	while(one != nullptr && two != nullptr){
+		if (one->number < two->number){
+			if (ret == nullptr){
+				node* tmp = new node;
+				tmp->number = one->number;
+				ret = tmp;
+				head = tmp;
+			} else {
+				node* tmp = new node;
+				tmp->number = one->number;
+				ret->next = tmp;
+				ret = ret->next;
+			}
+			one = one->next;
+		} else {
+			if (ret == nullptr){
+				node* tmp = new node;
+				tmp->number = two->number;
+				ret = tmp;
+				head = tmp;
+			} else {
+				node* tmp = new node;
+				tmp->number = two->number;
+				ret->next = tmp;
+				ret = ret->next;
+			}
+			two = two->next;
+		}
+	}
+	while(one != nullptr){
+		if (ret == nullptr){
+			node* tmp = new node;
+			tmp->number = one->number;
+			ret = tmp;
+			head = tmp;
+		} else {
+			node* tmp = new node;
+			tmp->number = one->number;
+			ret->next = tmp;
+			ret = ret->next;
+		}
+		one = one->next;
+	}
+	while(two != nullptr){
+		if (ret == nullptr){
+			node* tmp = new node;
+			tmp->number = two->number;
+			ret = tmp;
+			head = tmp;
+		} else {
+			node* tmp = new node;
+			tmp->number = two->number;
+			ret->next = tmp;
+			ret = ret->next;
+		}
+		two = two->next;
+	}
+	return head;
+}
 
 
-// nathan's output to file function
+void parenthesis(string &s) //replaces the brackets and squiggly brackets with parenthesis to operate
+{
+    int count = 0;
+    for (char c : s)
+    {
+        if (c == '[' || c == '{')
+        {
+            s.replace(count, 1, "(");
+        }
+        else if (c == ']' || c == '}')
+        {
+            s.replace(count, 1, ")");
+        }
+        count++;
+    }
+    return;
+}
+
+void remove_opp(node*& first, vector<int>& first_v){
+	// checks head for opp value
+	if (first == nullptr)
+		return;
+	for (int x = 0; x < first_v.size(); x++){
+		if (first_v[x] + first->number == 0){
+			first_v.erase(first_v.begin()+x);
+			first = first->next;
+			break;
+		}
+	}
+	node* headcpy = first;
+	node* temp = first;
+	temp = temp->next;
+	// checks everything else for opp value
+	while(temp != nullptr){
+		bool skip = false;
+		for (int x = 0; x < first_v.size(); x++){
+			if (first_v[x] + temp->number == 0){
+				first_v.erase(first_v.begin()+x);
+				temp = temp->next;
+				headcpy->next = temp;
+				skip = true;
+				break;
+			}
+		}
+		if (!skip){
+			temp = temp->next;
+			headcpy = headcpy->next;
+		}
+	}
+}
+
+bool checkIn(node* head, int target){
+	while(head != nullptr){
+		//cout << head->number << endl;
+		if (head->number == target){
+			return false;
+		}
+		head = head->next;
+	}
+	return true;
+}
 
 int main(int argc, char* argv[]){
 	if (argc < 2){
@@ -264,80 +333,125 @@ int main(int argc, char* argv[]){
 		return 0;
 	}
 	// argument manager stuff
-	 ArgumentManager am(argc, argv);
-	 string input = am.get("input");
-	 string output = am.get("output");
-	 ifstream ifs(input);
-	 ofstream ofs(output);
+	ArgumentManager am(argc, argv);
+	string input = am.get("input");
+	string output = am.get("output");
+	ifstream ifs(input);
+	ofstream ofs(output);
 
-	// ifstream ifs("input11.txt");
-	// ofstream ofs("output.txt");
-
-	node *travi=nullptr; //for travis
-	node *scarlet=nullptr; //for scarlet
+	vector<int> neg_t, neg_s;
+	node* travi=nullptr; //for travis
+	node* orderT=nullptr;
+	node* orderS=nullptr;
+	node* scarlet=nullptr; //for scarlet
 	node* together=nullptr; //for both
 
-	string line="";
-	bool travis = false, scarlett = false, valid = false;
+	bool travis = false, scarlett = false;
 	int invalid = 0;
+	string passcode;
 	while(!ifs.eof()){
-		getline(ifs, line);
-		cout << line << endl;
-		// if the line equal travis then we keep on adding to travis until it is scarlett
-		if (line == "Travis") { travis = true; scarlett = false; }
-		else if (line == "Scarlet") { scarlett = true; travis = false; }
-		// if the line is not a name then we check if it is invalid or not. if it is valid then we set valid to true which allows us to add to linkedlists
-		else if (expressValid(line) && line.length() > 0 && travis){ 
-			valid = true; 
-			//createlist(&travi, operation(line));
+		string filetext="";
+		string line = "";
+		getline(ifs, filetext);
+		if (filetext.length() == 0)
+			continue;
+		for (int x = 0; x < filetext.length(); x++){
+			if (filetext[x] != ' ' && filetext[x] != '\n' && filetext[x] != '\t')
+				line += filetext[x];
 		}
-		else if(expressValid(line) && line.length() > 0 && scarlett)
-			//createlist(&scarlet, operation(line));
 
-		if (travis && valid){
-			// add to travis
+		if (line.substr(0, 8) == "Passcode"){
+			passcode = line.substr(9, line.length() - 9);
+			break;
 		}
-		if (scarlett && valid){
-			// add to scarlett
-		} else if(!expressValid(line)){
-			// incrementing invalid count
+		if (line == "Travis") { 
+			travis = true; 
+			scarlett = false; 
+		} else if (line == "Scarlet") { 
+			scarlett = true; 
+			travis = false; 
+		} else if (expressValid(line) && line.length() > 0 && travis){ 
+			parenthesis(line);
+			if(operation(line) >= 0){
+				createlist(&travi, operation(line));
+				addNode(orderT, operation(line));
+			} else {
+				neg_t.push_back(operation(line));
+			}
+		}
+		else if(expressValid(line) && line.length() > 0 && scarlett){
+			parenthesis(line);
+			if (operation(line) >= 0){
+				createlist(&scarlet, operation(line));
+				addNode(orderS, operation(line));
+			} else {
+				neg_s.push_back(operation(line));
+			}
+		} else {
 			invalid++;
 		}
 	}
-	cout << "Invalid: " << invalid << endl;
 
-	
-	//testing deletion of negative/positive pairs
-	string s="6 8 9 3 7 9 2 -6 4 8";
-	stringstream b(s);
-	string u;
-	while(b){
-		getline(b, u, ' ');
-		createlist(&scarlet, stoi(u));
-		if(b.eof())
-			break;
-	}
-	string t= "-9 4 9 4 -8 -3 7 2 3";
-	stringstream ss(t); 
-	string m;
-	while(ss){
-		getline(ss, m, ' ');
-		createlist(&together, stoi(m));
-		if(ss.eof())
-			break;
-	}
-	checkneg(&together, &scarlet);
-	
-	// remember to delete index (amount of invalid) if greater than zero
-	int count=1; //to set counter for delete at pos (basically set position at 1)
-	delete_invalid(2, &together, count);
+	vector<int> neg_tc = neg_t;
+	vector<int> neg_sc = neg_s;
 
-	cout<<"Scarlet: ";
-	print(&scarlet);
-	cout<<endl;
-	cout<<"Travis:";
-	print(&travi);
-	cout<<endl<<"together: ";
+	// attempts to remove every neg from travis vector. if the vector isn't all empty then we need to try other scarlet
+	remove_opp(orderT, neg_t);
+	if (neg_t.size() > 0)
+		remove_opp(orderS, neg_t);
+
+	remove_opp(orderS, neg_s);
+	if (neg_s.size() > 0)
+		remove_opp(orderT, neg_s);
+
+	remove_opp(travi, neg_tc);
+	if (neg_tc.size() > 0)
+		remove_opp(scarlet, neg_tc);
+
+	remove_opp(scarlet, neg_sc);
+	if (neg_sc.size() > 0)
+		remove_opp(travi, neg_sc);
+
+
+	together = merge(travi, scarlet);
 	print(&together);
+
+	// deletes the index of invalid
+	delete_invalid(invalid, together, 1);
+	string t = "", s = "", decodedpass = "", actualpass = "";
+
+	while(orderT != nullptr){
+		t += to_string(orderT->number);
+		if (orderT->next != nullptr)
+			t += ',';
+		orderT = orderT->next;
+	}
+
+	while(orderS != nullptr){
+		s += to_string(orderS->number);
+		if (orderS->next != nullptr)
+			s += ',';
+		orderS = orderS->next;
+	}
+	ofs << "Scarlet: [" << s << "]" << endl;
+	ofs << "Travis: [" << t << "]" << endl;
+	decodedpass += "|";
+	while(together != nullptr){
+		decodedpass += " " + to_string(together->number) + " |";
+		together = together->next;
+	}
+
+	actualpass += "|";
+	for (int x = 0; x < passcode.length(); x++){
+		actualpass += " " + to_string(passcode[x]-'0') + " |";
+	}
+	ofs << "Decoded passcode: ";
+	ofs << decodedpass << endl;
+	ofs << "Actual passcode: ";
+	ofs << actualpass << endl;
+	if (actualpass == decodedpass)
+		ofs << "Treasure unlocked!";
+	else
+		ofs << "Treasure blocked!";
 	return 0;
 }
